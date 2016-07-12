56ef53603d17a7629cfefc47620427b391c82288
Colin <Colin@DESKTOP-QU472B1>
2016-07-11 22:58:17 -0500

    Database stuff.

--- TanBot.Application/src/gay_manager.c
+++ TanBot.Application/src/gay_manager.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <sql.h>
 
 #include "gay_manager.h"
 
---
+++ TanBot.Database/GayCounter.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[GayCounter]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [username] NCHAR(10) NOT NULL, 
+    [gayCount] INT NOT NULL DEFAULT 0, 
+    [lastIncident] DATETIME NOT NULL
+)
